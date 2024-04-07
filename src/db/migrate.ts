236c6b410ddad4dfe899d70a152854e0bb7a7823
aderitorafael16 <aderitobento16@gmail.com>
2024-04-07 10:05:55 +0100

    feat: wip(drizzle-config)

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,8 +1,6 @@
-import { neon, neonConfig } from '@neondatabase/serverless'
+import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import { migrate } from 'drizzle-orm/neon-http/migrator'
-
-neonConfig.fetchConnectionCache = true
 
 if (!process.env.DIRECT_DATABASE_URL) {
   throw new Error('Invalid environment variable DIRECT_DATABASE_URL')
